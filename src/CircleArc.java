e427ea95a608deb7ae96cc78525c8f27f4bc460b
LULucard <LULucard@yandex.ru>
2024-12-19 10:31:30 +0300

    Task2 (commit 2)

--- src/CircleArc.java
+++ src/CircleArc.java
@@ -23,10 +23,8 @@ public class CircleArc {
     ) {
         Graphics2D g = (Graphics2D) gr;
 
-        // Ебаное сглаживание, чтобы была ровная дуга, иначе как будто ты после бухича недельного рисовал от руки
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Объявление градиента по горизонтали
         GradientPaint gradient = new GradientPaint(
                 arc.x - arc.radius, arc.y, startColor,
                 arc.x + arc.radius, arc.y, endColor
@@ -34,12 +32,11 @@ public class CircleArc {
 
         g.setPaint(gradient);
 
-        // Отрисовываем блядскую дугу и кидаем ей на ротан параметры
         g.draw(new Arc2D.Double(
-                arc.x - arc.radius, arc.y - arc.radius, // x и y верхнего левого угла прямоугольника
-                arc.radius * 2, arc.radius * 2,         // Ширина и высота
-                startAngle, arc.arcAngle,              // Начальный угол и угловой размер
-                Arc2D.OPEN                             // Тип дуги (открытая)
+                arc.x - arc.radius, arc.y - arc.radius,
+                arc.radius * 2, arc.radius * 2,
+                startAngle, arc.arcAngle,
+                Arc2D.OPEN
         ));
     }
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,12 +3,8 @@ import java.awt.*;
 
 public class MainWindow extends JPanel {
 
-    // здесь тоже накидали параметры на ротан уже третий раз, ФАкел забивает хеттрик. Можешь с ними поиграть
-    // посмотреть че получается, да и вообще во все параметры потыкай, чтобы понимать че делают
-    CircleArc taskArc = new CircleArc(400, 300, 100, 150); // Центр в (400, 300), радиус 100, дуга 180°
-    // С колорами тоже можно поиграть подзаебаться, но нахуй оно нам надо, как говорится работает не трогай нахуй
-    // или прод положишь
-    // Но ты просто чилловый джун, который снес прод и бэкап (А потом тебе Сеньор кабину)
+    CircleArc taskArc = new CircleArc(400, 300, 100, 150);
+
     final Color startColor = Color.BLUE;
     final Color endColor = Color.RED;
 
@@ -16,11 +12,9 @@ public class MainWindow extends JPanel {
         setPreferredSize(new Dimension(800, 600));
     }
 
-    // Вызываем блядский родительский метод, потому что это Джава рот ее наоборот и накидываем параметры
-    // для полной отрисовки
     @Override
     protected void paintComponent(Graphics gr) {
         super.paintComponent(gr);
-        CircleArc.drawInterpolatedArc(taskArc, startColor, endColor, 0, gr); // Начальный угол 0°
+        CircleArc.drawInterpolatedArc(taskArc, startColor, endColor, 0, gr);
     }
 }
